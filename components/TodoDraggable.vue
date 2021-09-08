fc4daed64b2cd7f266d0129789ff8167b59923d4
Adam <lingyunggie@gmail.com>
2021-09-09 01:50:14 +0800

    Todo App completed

---
+++ components/TodoDraggable.vue
@@ -0,0 +1,56 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="w-1/5"
+    @drop="onDrop($event, list)"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <div class="bg-gray-500 p-4 flex justify-between">
+      <h1 class="text-white text-2xl self-center">{{ title }}</h1>
+      <div class="shadow-inner text-center p-2 bg-white">
+        <b>{{ countListItem }}</b> <br />Projects
+      </div>
+    </div>
+    <div
+      v-for="element in listItem"
+      :key="element.id"
+      draggable="true"
+      class="bg-white p-5 border-b-2"
+      @dragstart="startDrag($event, element)"
+    >
+      {{ element.name }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: { String, default: '' },
+    items: { Object, default: [] },
+    list: { Number, default: null },
+  },
+
+  computed: {
+    listItem() {
+      return this.items.filter((item) => item.list === this.list)
+    },
+    countListItem() {
+      const total = this.items.filter((item) => item.list === this.list)
+      return total.length
+    },
+  },
+  methods: {
+    startDrag: (evt, item) => {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('itemID', item.id)
+    },
+    onDrop(evt, list) {
+      const itemID = evt.dataTransfer.getData('itemID')
+      const item = this.items.find((item) => item.id === parseInt(itemID))
+      item.list = list
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,90 @@
 <template>
-  <Tutorial/>
+  <div class="mx-20">
+    <div class="flex justify-between my-5">
+      <div class="my-5 w-1/3">
+        <div class="flex">
+          <input
+            v-model="newItem.name"
+            class="
+              shadow
+              appearance-none
+              border
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              leading-tight
+              focus:outline-none focus:shadow-outline
+            "
+          />
+          <button
+            @click="addItem"
+            class="
+              bg-green-500
+              hover:bg-green-700
+              text-white
+              font-bold
+              w-1/4
+              py-1
+              px-1
+              ml-5
+              rounded
+            "
+          >
+            Add Project
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div class="ml-auto text-center">
+          <label class="uppercase font-bold"> Total </label>
+          <div class="shadow-inner text-center p-2 bg-white">
+            <b>{{ totalItems }}</b> <br />Projects
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <todo-draggable title="To Do" :items="items" :list="1" />
+      <todo-draggable title="In Progress" :items="items" :list="2" />
+      <todo-draggable title="Done" :items="items" :list="3" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import TodoDraggable from '~/components/TodoDraggable.vue'
+export default {
+  data() {
+    return {
+      items: [
+        { name: 'Project 1', id: 1, list: 1 },
+        { name: 'Project 2', id: 2, list: 1 },
+        { name: 'Project 3', id: 3, list: 1 },
+        { name: 'Project 4', id: 4, list: 1 },
+        { name: 'Project 5', id: 5, list: 2 },
+        { name: 'Project 6', id: 6, list: 2 },
+        { name: 'Project 7', id: 7, list: 3 },
+        { name: 'Project 8', id: 8, list: 3 },
+        { name: 'Project 9', id: 9, list: 3 },
+      ],
+      newItem: { name: null, id: null, list: null },
+    }
+  },
+  components: { TodoDraggable },
+  computed: {
+    totalItems() {
+      return this.items.length
+    },
+  },
+  methods: {
+    addItem() {
+      this.newItem.id = this.items.length + 1
+      this.newItem.list = 1
+      return this.items.push({ ...this.newItem })
+    },
+  },
+}
 </script>
